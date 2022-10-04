7b8de657979396f7bac968bf54e8d6e872a1f0a3
academyoffice79 <101295533+academyoffice79@users.noreply.github.com>
2022-10-04 18:41:56 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -2,8 +2,14 @@
 function openNav() {
     document.getElementById("myNav").style.width = "100%";
   }
+
+  /* Open when someone clicks on the span element */
+function openTopics() {
+  document.getElementById("myTopicNav").style.width = "100%";
+}
   
-  /* Close when someone clicks on the "x" symbol inside the overlay */
-  function closeNav() {
-    document.getElementById("myNav").style.width = "0%";
-  }
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closeNav() {
+  document.getElementById("myNav").style.width = "0%";
+  document.getElementById("myTopicNav").style.width = "0%";
+}
